ffc5db9857c32f9acdc509a499d2985d1152d908
LaanFor <niko.sky.lan@gmail.com>
2020-02-28 23:06:53 +0800

    init post

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,57 +1,67 @@
 module.exports = {
-  pathPrefix: '/',
+  pathPrefix: "/",
   siteMetadata: {
-    author: 'Ken Powers',
-    title: 'Ken Powers',
-    siteUrl: 'https://knpw.rs',
-    description: 'With Ken Powers Comes Ken Responsibility',
+    author: "Laanfor",
+    title: "Laanfor",
+    siteUrl: "https://laanfor.cc",
+    description: "laanfor`s blog"
   },
   plugins: [
-    'gatsby-plugin-catch-links',
-    'gatsby-plugin-emotion',
-    'gatsby-plugin-favicon',
-    'gatsby-plugin-netlify',
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sharp', {
-      resolve: 'gatsby-source-filesystem',
+    "gatsby-plugin-catch-links",
+    "gatsby-plugin-emotion",
+    "gatsby-plugin-favicon",
+    "gatsby-plugin-netlify",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/pages`,
-        name: 'pages',
-      },
-    }, {
-      resolve: 'gatsby-plugin-build-date',
+        name: "pages"
+      }
+    },
+    {
+      resolve: "gatsby-plugin-build-date",
       options: {
         formatting: {
-          format: 'YYYY',
-        },
-      },
-    }, {
-      resolve: 'gatsby-transformer-remark',
+          format: "YYYY"
+        }
+      }
+    },
+    {
+      resolve: "gatsby-transformer-remark",
       options: {
-        plugins: [{
-          resolve: 'gatsby-remark-prismjs',
-          options: {
-            classPrefix: 'language-',
+        plugins: [
+          {
+            resolve: "gatsby-remark-prismjs",
+            options: {
+              classPrefix: "language-"
+            }
           },
-        }, {
-          resolve: 'gatsby-remark-images',
-          maxWidth: 950,
-        }],
-      },
-    }, {
-      resolve: 'gatsby-plugin-google-analytics',
+          {
+            resolve: "gatsby-remark-images",
+            maxWidth: 950
+          }
+        ]
+      }
+    },
+    {
+      resolve: "gatsby-plugin-google-analytics",
       options: {
-        trackingId: 'UA-46190412-1',
-      },
-    }, {
-      resolve: 'gatsby-plugin-sitemap',
-    }, {
-      resolve: 'gatsby-plugin-canonical-urls',
+        trackingId: "UA-46190412-1"
+      }
+    },
+    {
+      resolve: "gatsby-plugin-sitemap"
+    },
+    {
+      resolve: "gatsby-plugin-canonical-urls",
       options: {
-        siteUrl: 'https://knpw.rs',
-      },
-    }, {
-      resolve: 'gatsby-plugin-feed',
+        siteUrl: "https://laanfor.cc"
+      }
+    },
+    {
+      resolve: "gatsby-plugin-feed",
       options: {
         query: `{
           site {
@@ -62,16 +72,18 @@ module.exports = {
             }
           }
         }`,
-        feeds: [{
-          serialize: ({ query: { site, allMarkdownRemark } }) => allMarkdownRemark.edges.map(
-            edge => Object.assign({}, edge.node.frontmatter, {
-              description: edge.node.html,
-              url: site.siteMetadata.siteUrl + edge.node.frontmatter.path,
-              guid: site.siteMetadata.siteUrl + edge.node.frontmatter.path,
-              custom_elements: [{ 'content:encoded': edge.node.html }],
-            }),
-          ),
-          query: `{
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) =>
+              allMarkdownRemark.edges.map(edge =>
+                Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.html,
+                  url: site.siteMetadata.siteUrl + edge.node.frontmatter.path,
+                  guid: site.siteMetadata.siteUrl + edge.node.frontmatter.path,
+                  custom_elements: [{ "content:encoded": edge.node.html }]
+                })
+              ),
+            query: `{
             allMarkdownRemark(
               limit: 1000,
               sort: { order: DESC, fields: [frontmatter___date] }
@@ -88,8 +100,10 @@ module.exports = {
               }
             }
           }`,
-          output: '/rss.xml',
-        }],
-      },
-    }],
+            output: "/rss.xml"
+          }
+        ]
+      }
+    }
+  ]
 };
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,65 +1,62 @@
 /* eslint-disable jsx-a11y/accessible-emoji */
-import React from 'react';
-import styled from '@emotion/styled';
-import { Link, A } from './header-footer-anchor';
+import React from "react";
+import styled from "@emotion/styled";
+import { Link, A } from "./header-footer-anchor";
 
 const Header = styled.header(({ theme }) => ({
   padding: `0 ${theme.spacing}`,
-  display: 'flex',
-  flexDirection: 'row',
-  justifyContent: 'space-between',
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-between",
   height: theme.headerHeight,
   lineHeight: theme.headerHeight,
   color: theme.textColor,
   [theme.smallMedia]: {
-    flexDirection: 'column',
-    textAlign: 'center',
-    justifyContent: 'space-between',
-    height: '50px',
-    lineHeight: '20px',
+    flexDirection: "column",
+    textAlign: "center",
+    justifyContent: "space-between",
+    height: "50px",
+    lineHeight: "20px"
   },
   [theme.largeMedia]: {
-    ...theme.centerPadding,
-  },
+    ...theme.centerPadding
+  }
 }));
 
 const H1 = styled.h1(({ theme }) => ({
-  fontSize: '1.25rem',
+  fontSize: "1.25rem",
   margin: 0,
   [theme.smallMedia]: {
-    fontSize: '1rem',
-  },
+    fontSize: "1rem"
+  }
 }));
 
 const Small = styled.small({
-  fontSize: '75%',
-  opacity: 0.35,
+  fontSize: "75%",
+  opacity: 0.35
 });
 
 const Nav = styled.nav(({ theme }) => ({
-  display: 'flex',
-  flexDirection: 'row',
-  justifyContent: 'space-between',
-  textTransform: 'uppercase',
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-between",
+  textTransform: "uppercase",
   a: {
-    marginLeft: theme.spacing,
-  },
+    marginLeft: theme.spacing
+  }
 }));
 
 const SiteHeader = () => (
   <Header>
     <H1>
-      <Small>With</Small>
-      {' '}
-      <Link to="/">Ken Powers</Link>
-      {' '}
-      <Small>comes Ken Responsibility</Small>
+      <Small>With</Small> <Link to="/">似号</Link>{" "}
+      <Small>枪炮、病菌与键盘</Small>
     </H1>
     <Nav>
       <Link to="/">Blog</Link>
-      <Link to="/about">About</Link>
-      <A href="https://github.com/knpwrs">GitHub</A>
-      <A href="https://twitter.com/knpwrs">Twitter</A>
+      <Link to="/about">关于</Link>
+      <A href="https://github.com/nikomaniac">GitHub</A>
+      <A href="https://twitter.com/4sihao">Twitter</A>
     </Nav>
   </Header>
 );
